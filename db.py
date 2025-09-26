604e5ac2ac891b335e4d3430d134db650eb6c3ff
igorpszczola1 <251208@edu.p.lodz.pl>
2025-09-26 14:22:26 +0200

    Stop tracking IDE and db files, update gitignore

--- db.py
+++ db.py
@@ -178,7 +178,6 @@ def list_not_done():
     try:
         cursor.execute("SELECT * FROM Todo WHERE Status = 'Pending' OR Status = 'Pending(Late)'")
         data = cursor.fetchall()
-        return data
         # print_rows(data)
     except sqlite3.Error as error:
         print('Failed to list not done tasks, error: ', error)
